935739dd9d6ad70f049920e60af8e91e3290b0ee
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-07-03 21:35:41 +0200

    add key to UserList.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import AddUser from "./components/Users/AddUser";
 import UserList from "./components/Users/UsersList";
-
+import React, { useState } from "react";
 function App() {
+  const [userList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUserList) => {
+      return [...prevUserList, { name: uName, age: uAge , id:Math.random().toString()}];
+    });
+  };
   return (
     <div className="App">
-      <AddUser />
-      <UserList users={[]} />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userList} />
     </div>
   );
 }
--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -2,15 +2,14 @@ import Card from "../UI/Card";
 import styles from './UserList.module.css'
 const UsersList = (props) => {
   return (
-    <Card className={styles.user}>
+    <Card className={styles.users}>
       <ul>
         {props.users.map((user) => (
-          <li>
+          <li key={user.id}>
             {user.name} ({user.age} years old)
           </li>
         ))}
       </ul>
-      ;
     </Card>
   );
 };
